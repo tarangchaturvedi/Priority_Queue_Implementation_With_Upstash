8d626e0c364b0daef947caf2166d4e030769164b
tarangchaturvedi <tarang.chaturvedi11@gmail.com>
2025-01-02 20:49:49 +0530

    Update QueueService.java

--- src/main/java/com/example/QueueService.java
+++ src/main/java/com/example/QueueService.java
@@ -5,7 +5,7 @@ public interface QueueService {
   public void push(String queueUrl, String messageBody, int priority);
 
   /** retrieves a single message from a queue. */
-  public Message pull(String queueUrl); // toremove variable ensures if retrieved message is to be removed from queue or not.
+  public Message pull(String queueUrl);
 
   /** deletes a message from the queue that was received by pull(). */
   public void delete(String queueUrl, String receiptId);
